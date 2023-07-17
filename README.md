c886ffd286287f8860990020b21260282b69d75b
Aladdin4u <taofikakinkumi@gmail.com>
2023-07-17 04:43:04 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # icodeThis
+Daily front-end coding challenge to improve skills and abillity
+
+## How It's Made:
+
+**Tech used:**<p>![HTML5 BADGE](https://img.shields.io/static/v1?label=|&message=HTML5&color=23555f&style=plastic&logo=html5) ![CSS BADGE](https://img.shields.io/static/v1?label=|&message=CSS3&color=green&style=plastic&logo=css3) [JAVASCRIPT](https://img.shields.io/static/v1?label=|&message=JAVASCRIPT&color=yellow&style=plastic&logo=JAVASCRIPT)</p>
---
+++ keyboard/main.js
@@ -0,0 +1,46 @@
+const btns = document.querySelectorAll(".letter");
+const btnEl = Array.from(btns);
+const display = document.getElementById("display");
+const exit = document.getElementById("exit");
+const upperCase = document.getElementById("uppercase");
+const space = document.getElementById("space");
+console.log(document.getElementsByTagName("input"))
+btnEl.forEach(btn => 
+    btn.addEventListener("click", e => {
+        const char = e.target.textContent.toLowerCase()
+        console.log("Button handler",e.target.textContent.toLowerCase());
+        console.log(document.getElementsByTagName("input"))
+        display.value += e.target.textContent;
+        // display.textContent.value += e.target.textContent
+    })
+)
+
+upperCase.addEventListener("click", e => {
+    console.log("uppercase", e.target)
+    if(e.target.classList.contains("dark")) {
+        console.log("dark exist")
+        e.target.classList.remove("dark")
+        btns.forEach(btn => {
+            const btnEl = btn.textContent;
+            // console.log("change btn", btn.textContent.toLowerCase())
+            return btn.textContent = btn.textContent.toUpperCase()
+        })
+    } else {
+        e.target.classList.add("dark")
+        console.log("not dark exist")
+        btns.forEach(btn => {
+            const btnEl = btn.textContent;
+            // console.log("change btn", btn.textContent.toLowerCase())
+            return btn.textContent = btn.textContent.toLowerCase()
+        })
+    }
+    
+})
+exit.addEventListener("click", e => {
+    const displayEl = Array.from(display.textContent);
+    displayEl.pop()
+    display.textContent = displayEl.join("")
+})
+space.addEventListener("click", e => {
+    display.value += " ";
+})
